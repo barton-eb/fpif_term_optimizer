95a04b8d371d2831a19b0ea1c62d2e08726f3056
barton-eb <33558490+barton-eb@users.noreply.github.com>
2017-11-10 16:56:07 -0600

    still figuring out git

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ Created on Fri Nov 10 14:25:24 2017
 
 @author: Barton
 
-@purpose: To input a set of terms for the FPI/F defense contract type and generate all possible 'equivalent scenarios.' Histograms
-            will be generated from the results.
+@purpose: To input a set of terms for the FPI/F defense contract type and generate all possible 'equivalent scenarios.' 
+			Histograms will be generated from the results.
 """
 from fpi_functions import calculate_fpi_ros
 
